eaecc34afcd5c9c841d2624e142a4835da5e4c1f
ShaJaPas <shajapas@gmail.com>
2025-06-10 13:47:34 +0300

    Set copyKernels to false (reduce boot time)

--- profiles/work/configuration.nix
+++ profiles/work/configuration.nix
@@ -1,7 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 {
   pkgs,
   lib,
@@ -66,7 +62,7 @@
      # efiInstallAsRemovable = true; # Otherwise /boot/EFI/BOOT/BOOTX64.EFI isn't generated
       device = systemSettings.grubDevice;
       extraEntriesBeforeNixOS = false;
-      copyKernels = true;
+      copyKernels = false;
       useOSProber = false;
     };
   };
