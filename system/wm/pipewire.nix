0254afb5fdf0a8dd429c39290ea4a005e0fce393
ShaJaPas <shajapas@gmail.com>
2025-07-18 17:07:37 +0300

    FIX: disable pulse, so the sound settings will be saved after reboot

--- system/wm/pipewire.nix
+++ system/wm/pipewire.nix
@@ -7,13 +7,7 @@
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
-    pulse.enable = true;
+    pulse.enable = false;
     jack.enable = true;
-    extraConfig.pipewire = {
-      "context.properties" = {
-        "default.configured.audio.source" =
-          "{ \"name\": \"alsa_card.usb-Generalplus_Usb_Audio_Device-00\" }";
-      };
-    };
   };
 }
