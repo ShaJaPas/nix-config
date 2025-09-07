8c27c3679c45d69c0968726950b8c3f826c31f6d
ShaJaPas <shajapas@gmail.com>
2025-09-07 09:49:35 +0300

    Disable audio suspension

--- system/wm/general/pipewire.nix
+++ system/wm/general/pipewire.nix
@@ -9,5 +9,24 @@ _:
     alsa.support32Bit = true;
     pulse.enable = true;
     jack.enable = true;
+    wireplumber.extraConfig."99-disable-suspend" = {
+      "monitor.alsa.rules" = [
+        {
+          matches = [
+            {
+              "node.name" = "~alsa_input.*";
+            }
+            {
+              "node.name" = "~alsa_output.*";
+            }
+          ];
+          actions = {
+            update-props = {
+              "session.suspend-timeout-seconds" = 0;
+            };
+          };
+        }
+      ];
+    };
   };
 }
